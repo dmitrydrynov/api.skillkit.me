4d64cf0b0bbe38bc07d43423e8fd52610689d777
Dmitry Drynov <dmitry.drynov@gmail.com>
2022-07-08 22:14:46 +0300

    Update index page

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,18 @@ export const routes: Route = {
   get: {
     handler: async (_request, reply) => {
       reply.type('text/html; charset=utf-8');
-      await reply.send('GDHUB API v0.0.1-alpha');
+      await reply.send(`
+        <html>
+        <head>
+          <title>Skillkit⚡API</title>
+          <meta name="robots" content="noindex, nofollow" />
+          <link href="https://fonts.googleapis.com/css?family=Rubik" rel="stylesheet">
+        </head>
+        <body style="font-family: 'Rubik'; display: flex; justify-content: center; align-items: center;margin: 0; position: absolute; width: 100%; height: 100%">
+        <div style="font-size: 24px;">Skillkit⚡API</div>
+        </body>
+        </html>
+      `);
     },
   },
 };
