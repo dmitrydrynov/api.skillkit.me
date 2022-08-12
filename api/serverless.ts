20e39db5bc79f5f0c53d0c8c05a5247bc3b3eb13
Dmitry Drynov <dmitry.drynov@gmail.com>
2022-08-12 14:49:34 +0300

    Fix vercel servless

--- api/serverless.ts
+++ api/serverless.ts
@@ -1,7 +1,32 @@
-import { app } from '../src/server';
+import { env } from '@config/env';
+import closeWithGrace from 'close-with-grace';
+import Fastify, { FastifyInstance } from 'fastify';
+
+export const app: FastifyInstance = Fastify({
+  ignoreTrailingSlash: true,
+  trustProxy: true,
+  logger: {
+    level: env.LOG_LEVEL,
+    prettyPrint: env.LOG_PRETTY_PRINT,
+  },
+});
+
+// Register your application as a normal plugin.
+app.register(import('../src/app'), { prefix: '/' });
+
+const closeListeners = closeWithGrace({ delay: 500 }, async function ({ err }) {
+  if (err) {
+    console.error(err);
+  }
+  await app.close();
+});
+
+app.addHook('onClose', async (instance, done) => {
+  closeListeners.uninstall();
+  done();
+});
 
 export default async (req, res) => {
   await app.ready();
-
-  return app.server.emit('request', req, res);
+  app.server.emit('request', req, res);
 };
