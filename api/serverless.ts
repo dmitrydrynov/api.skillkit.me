efc7f8a952558fb20d04e1390485591a8a3bbfcd
Dmitry Drynov <dmitry.drynov@gmail.com>
2022-08-12 15:14:43 +0300

    Test vercel serverless

--- api/serverless.ts
+++ api/serverless.ts
@@ -1,38 +1,4 @@
-import { env } from '@config/env';
-import closeWithGrace from 'close-with-grace';
-import Fastify, { FastifyInstance } from 'fastify';
-
-export const app: FastifyInstance = Fastify({
-  ignoreTrailingSlash: true,
-  trustProxy: true,
-  logger: {
-    level: env.LOG_LEVEL,
-    prettyPrint: env.LOG_PRETTY_PRINT,
-  },
-});
-
-// Register your application as a normal plugin.
-app.register(import('../src/app'), { prefix: '/' });
-
-const closeListeners = closeWithGrace({ delay: 500 }, async function ({ err }) {
-  if (err) {
-    console.error(err);
-  }
-  await app.close();
-});
-
-app.addHook('onClose', async (instance, done) => {
-  closeListeners.uninstall();
-  done();
-});
-
-const start = async () => {
-  try {
-    await app.listen(env.PORT, env.HOST);
-    app.log.info('Fastify v' + app.version + ' started');
-  } catch (err: any) {
-    app.log.error(err);
-    process.exit(1);
-  }
-};
-start();
+export default function handler(request, response) {
+  const { name } = request.query;
+  response.status(200).send(`Hello ${name}!`);
+}
