6f6bd9c396e855f47809f4092e707039f3ce725a
Dmitry Drynov <dmitry.drynov@gmail.com>
2022-08-12 11:45:29 +0300

    Add vercel config

---
+++ api/serverless.js
@@ -0,0 +1,19 @@
+// Require the framework
+import Fastify from 'fastify';
+// Read the .env file.
+import * as typedDotenv from 'typed-dotenv';
+
+typedDotenv.config({ debug: false });
+
+// Instantiate Fastify with some config
+const app = Fastify({
+  logger: true,
+});
+
+// Register your application as a normal plugin.
+app.register(import('../src/server'));
+
+export default async (req, res) => {
+  await app.ready();
+  app.server.emit('request', req, res);
+};
